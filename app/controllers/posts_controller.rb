b56affcd8fe63cd75388a4b4d630cc3ed00a2d31
vgty <victor.goutay@gmail.com>
2017-02-13 14:28:46 +0200

    Adding validations to Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,16 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
   def new
+    @post = Post.new
   end
   def create
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+    
+    if @post.save
+      redirect_to @post
+    else
+    render 'new'
+    end
   end
 end
 private
